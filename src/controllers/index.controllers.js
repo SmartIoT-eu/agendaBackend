1a016373f37152d4f95e6dd5b52874f5adbbf34b
Alejandro Martín <alexandermpf77@gmail.com>
2019-10-12 17:20:43 +0200

    getUser, createUsers, deleteUser, updateUser

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -11,15 +11,50 @@ const pool = new Pool({
 
 const getUsers = async (req, res) => {
     const resposone = await pool.query('SELECT * FROM users');
-    //console.log(resposone.rows);
     res.status(200).json(resposone.rows);
 }
 
+const getUser = async (req, res) => {
+    const id = req.params.id;
+    const response = await pool.query('SELECT * FROM users WHERE id = $1', [id])
+    res.json(response.rows);
+}
+
 const createUsers = async (req, res) => {
-    //const respones = await pool.query('');
+    const { name, email } = req.body;
+    const response = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email]);
+    console.log(response);
+    res.json({
+        message: 'User Added Succesfully',
+        body: {
+            user: {name, email}
+        }
+    })
+}
+
+const deleteUser = async (req, res) => {
+    const id = req.params.id;
+    const response = await pool.query('DELETE FROM users WHERE id = $1', [id]);
+    console.log(response);
+    res.json(`User ${id} deleted successfully`);
+}
+
+const updateUser = async(req, res) => {
+    const id = req.params.id;
+    const { name, email } = req.body;
+    const response = await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3', [
+        name,
+        email,
+        id
+    ])
+    console.log(response);
+    res.send('User Updated Succefully');
 }
 
 module.exports = {
     getUsers,
-    createUsers
+    getUser,
+    createUsers,
+    deleteUser,
+    updateUser
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getUsers, createUsers } = require('../controllers/index.controllers');
+const { getUsers, getUser, createUsers, deleteUser, updateUser } = require('../controllers/index.controllers');
 
 router.get('/users', getUsers);
+router.get('/users/:id', getUser);
 router.post('/users', createUsers);
+router.delete('/users/:id', deleteUser);
+router.put('/users/:id', updateUser);
 
 module.exports = router;
